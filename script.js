34e1b2d76598d8f5eda0264e3ff847072595abc7
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-01-20 16:36:40 -0500

    linked to google maps API and put up basic map

---
+++ script.js
@@ -0,0 +1,9 @@
+function initMap(){
+  var options = {
+    zoom:12,
+    center:{lat:40.7831,lng:-73.9712}
+  };
+  // New map
+  var map = new google.maps.Map(document.getElementById('map'),options);
+
+};
