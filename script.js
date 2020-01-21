53adce717ab88ffb0154be96bef3d5440a5db7c9
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-01-21 14:42:16 -0500

    added get attraction functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var map, user, infoWindow;
 
 // append the key script
 
-$("body").append(`<script src="https://maps.googleapis.com/maps/api/js?key= + ${key} + &callback=initMap&libraries=places"
+$("body").append(`<script src="https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap&libraries=places"
 async defer></script>`)
 
 
@@ -42,3 +42,56 @@ function initMap(){
   }
 };
 
+
+// For if user's location can't be grabbed
+function handleLocationError (content, position){
+  errorWindow.setPosition(position);
+  errorWindow.setContent(content);
+  errorWindow.open(map);
+}
+// Request tourist attractions and place markers
+function getAttractions (location) {
+  var pyrmont = new google.maps.LatLng(location.lat, location.long);
+  var request = {
+      location: pyrmont,
+      radius: "1500",
+      type: ["tourist_attraction"]
+  };
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK){
+      console.log(results)
+      // look at result set
+      // sort by rating
+      // set a variable that contains the top three
+      // below, iterate over new variable instead of original result set
+      var topSorted = results.slice(0, 4) 
+      for (var i = 0; i < topSorted.length; i++) {
+          var place = topSorted[i];
+
+          let content = `<h3>${place.name}</h3>
+          <h4>${place.vicinity}</h4>
+
+          Rating: ${place.rating}
+          </p>`;
+          var marker = new google.maps.Marker({
+              position: place.geometry.location,
+              map: map,
+              title: place.name
+          });
+          var infowindow = new google.maps.InfoWindow({
+              content: content
+          });
+          bindInfoWindow(marker, map, infowindow, content);
+          marker.setMap(map);
+      }
+  }
+}
+function bindInfoWindow (marker, map, infowindow, html) {
+  marker.addListener("click", function() {
+      infowindow.setContent(html);
+      infowindow.open(map, this);
+  });
+}
