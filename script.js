43ced5ac2d5585f75a26340f9d5c6a7038b361d1
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-01-20 22:26:47 -0500

    Get location permission and place user on map

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+var map, user, infoWindow;
+
 function initMap(){
   var options = {
     zoom:12,
     center:{lat:40.7831,lng:-73.9712}
   };
   // New map
-  var map = new google.maps.Map(document.getElementById('map'),options);
-
+  map = new google.maps.Map(document.getElementById('map'),options);
+  // Info Window for location error
+  infoWindow = new google.maps.InfoWindow;
+  // Add marker
+  user = new google.maps.Marker({
+    map:map,
+    title: 'You are here!',
+    icon:'https://www.google.com/mapfiles/arrow.png'
+  })
+  // Get user's location and put on map, give error msg otherwise
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (p) {
+      var position = {
+        lat: p.coords.latitude,
+        lng: p.coords.longitude
+      };
+      user.setPosition(position);
+    }, function() {
+      handleLocationError('Geolocation service failed', map.center());
+    })
+  } else {
+    handleLocationError ('No geolocation available', map.center());
+  }
+}
+function handleLocationError (content, position){
+  info
 };
