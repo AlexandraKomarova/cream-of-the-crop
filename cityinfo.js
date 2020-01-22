1da2ee86a628b95705130a7db6759be33b997b15
gracejl10157 <gracejl10157@gmail.com>
2020-01-22 13:38:49 -0500

    city info

---
+++ cityinfo.js
@@ -0,0 +1,62 @@
+
+var region =[hudsonSquare, soHo, noLita, littleItaly, bowery, lowerEastSide, triBeCa, chinatown, civicCenter, twoBridges, batteryParkCity, financialDistrict]
+
+var hudsonSquare = "Within the neighborhood is the landmarked Charlton–King–Vandam Historic District, which contains the largest concentration of Federalist and Greek Revival style row houses built during the first half of the 19th century. The most prominent feature within the neighborhood is the Manhattan entrance to the Holland Tunnel. The tallest structure in the neighborhood is the Dominick condo hotel. ";
+var hudsonImage = "https://imgs.6sqft.com/wp-content/uploads/2017/07/24104001/565-broome-04.jpg"
+var hudsonName = "Hudson Square"
+
+
+var soHo = "Designer boutiques, fancy chain stores and high-end art galleries make trendy SoHo a top shopping destination, especially for out-of-towners. Known for its elegant cast-iron-facades and cobblestone streets, the neighborhood is also an atmospheric backdrop for fashionable crowds clustering at high-end restaurants and nightlife hotspots. During the day, street vendors sell everything from jewelry to original artwork.";
+var sohoImage ="https://d.newsweek.com/en/full/1404341/soho.webp?w=737&f=df29cc3130f9e84ff3528819edc057ed"
+var sohoName = "SoHo"
+
+
+var noLita = "Picturesque Nolita is a charming, upscale area with a trendy vibe. It's known for its chic shopping scene, and has plenty of designer jewelry shops, unique clothing boutiques and home-design stores. Fashionably casual crowds stroll the neighborhood and fill the popular sidewalk cafes, chic bars and trendy restaurants. On weekends, street vendors selling hand-made jewelry and artwork line Prince Street.";
+var nolitaImage = "https://images.ctfassets.net/ypfe9l9zihcg/4WYcQwfzoIoK6EW4sYCgSy/c1648298992e240e0b179d74e8bf13cf/nolita_around_the_block_the_lifestyle_7.jpg?fm=jpg&fl=progressive&q=85&w=900"
+var nolitaName = "NoLita"
+
+
+var littleItaly = "Little Italy welcomes a heavily tourist crowd to its high concentration of souvenir shops and traditional Italian eateries and bakeries. Tenement buildings, once home to the immigrants who settled the area in the late 1800s, line the narrow streets. Mulberry Street, the main thoroughfare, turns into a pedestrian mall on summer weekends. The area celebrates its heritage each September at the busy San Gennaro festival.";
+var littleitalyImage = "https://assets3.thrillist.com/v1/image/2813007/size/gn-gift_guide_variable_c_2x.jpg"
+var littleitalyName ="Little Italy"
+
+
+var bowery = "In the 17th century, the road branched off Broadway north of Fort Amsterdam at the tip of Manhattan to the homestead of Peter Stuyvesant, director-general of New Netherland. The street was known as Bowery Lane prior to 1807. 'Bowery' is an anglicization of the Dutch bouwerij, derived from an antiquated Dutch word for 'farm': In the 17th century the area contained many large farms. The New York City Subway's Bowery station, serving the BMT Nassau Street Line (J and ​Z trains), is located close to the Bowery's intersection with Delancey and Kenmare Streets. There is a tunnel under the Bowery once intended for use by the proposed, but never built, New York City Subway services, including the Second Avenue Subway.";
+var boweryImage = "https://untappedcities-wpengine.netdna-ssl.com/wp-content/uploads/2017/07/Bowery-Market-Great-Jones-Street-East-Village-Food-Hall-NYC-002.jpg"
+var boweryName = "Bowery"
+
+
+var lowerEastSide = "The eclectic Lower East Side is where gritty alleys and tenement-style buildings mix with upscale apartments and chic boutiques. Nighttime draws hip, young crowds to the area's trendy bars, music venues and restaurants. The neighborhood's Jewish heritage lives on through Orchard Street's Lower East Side Tenement Museum and old-world fabric stores, as well as traditional delis such as Katz's and Russ & Daughters.";
+var lowereastsideImage = "https://www.tripsavvy.com/thmb/fNuVkCT67U7DIIfDEeeNIjxLmzQ=/3864x2576/filters:no_upscale():max_bytes(150000):strip_icc()/high-angle-view-of-lower-east-side-manhattan-downtown--new-york-city--usa-640006562-5c2015f846e0fb0001922d95.jpg"
+var lowereastsideName = "Lower East Side"
+
+
+var triBeCa = "Tribeca is a hip area known for its old industrial buildings, many now turned into residential loft space. Cobblestone streets are lined with trendy boutiques and restaurants. Historic commercial buildings include the red-brick New York Mercantile Exchange edifice, from 1884. Weekends are quiet, though Washington Market Park and Hudson River Park draw families. The Tribeca Film Festival takes place here every spring.";
+var tribecaImage = "https://loving-newyork.com/wp-content/uploads/2018/11/Things-To-Do-In-Tribeca-181012130243007-1600px-1600x960.jpg"
+var tribecaName= "TriBeCa"
+
+
+var chinatown = "Vibrant Chinatown is a densely populated neighborhood that draws foodies and tourists to its many Chinese and Southeast Asian restaurants for dumplings, pork buns and hand-pulled noodles. The busy sidewalks are packed with souvenir stores, bubble tea shops, and markets selling everything from fresh and dried fish to herbs and spices. Locals hang out in leafy Columbus Park for Tai Chi, chess and mahjong.";
+var chinatownImage= "https://imgs.6sqft.com/wp-content/uploads/2015/03/21002238/ChinatownNYC.jpg"
+var chinatownName = "Chinatown"
+
+
+var civicCenter = "Although government-related activities are predominant, other pursuits also occur within the district, including entertainment, industrial activity, residential dwellings, and warehousing. For example, there are Chinese restaurants near Civic Center's border with Chinatown, in addition to some museums and some residential buildings in the Civic Center area. The area is roughly 10 blocks long and 5 blocks wide, but is far less dense than most of Manhattan, where the average number of residents for an area that size is 35,000; the Civic Center has approximately 20,000 residents. The Jacob K. Javits Federal Building is located in the area. It includes the Federal Bureau of Investigation New York field office. ";
+var civiccenterImage = "https://static01.nyt.com/images/2010/01/31/realestate/31living_span-CA0/articleLarge.jpg"
+var civiccenterName ="Civic Center"
+
+
+var twoBridges = "Two Bridges is a neighborhood near the footings of the Brooklyn Bridge and Manhattan Bridge. The neighborhood has been considered to be a part of the Lower East Side for much of its history. Two Bridges has traditionally been an immigrant neighborhood, previously populated by immigrants from Europe, and more recently from Latin America and China. The Two Bridges Historic District was listed in the National Register of Historic Places in September 2003. Two Bridges has a mix of tenement style walk-up buildings and high-rise buildings that include mixed-income and affordable housing developments as well as public housing provided by the New York City Housing Authority (NYCHA).";
+var twobridgesImage ="https://upload.wikimedia.org/wikipedia/commons/a/a2/20170721_Gotham_Shield_NYC_Aerials-195_medium.jpg"
+var twobridgesName = "Two Bridges"
+
+
+var batteryParkCity = "Set along the Hudson River, Battery Park City is a mainly residential neighborhood of upscale high-rise apartment buildings. During the workweek, its casual eateries and bars draw professionals from the nearby Financial District. On weekends, the area slows down, though locals and visitors are drawn to the multitower Brookfield Place shopping center and the bike friendly paths of the riverfront park.";
+var batterypartcityImage ="https://cdn-img-feed.streeteasy.com/nyc/image/76/89954976.jpg"
+var batterypartcityName = "Battery Park City"
+
+
+var financialDistrict = "This is the city's buzzing financial heart, home to Wall Street and glittering skyscrapers. Sidewalks bustle during the week and, after work, young professionals fill the restaurants and bars of the South Street Seaport and pedestrian-only Stone Street. The sombre National September 11 Memorial and Museum, in the footprint of the Twin Towers, is also here. Above it all is the observatory atop One World Trade Center.";
+var financialdistrictImage = "https://thenypost.files.wordpress.com/2019/06/financial-district.jpg?quality=90&strip=all&w=1236&h=820&crop=1"
+var financialdistrictimageName = "Financial District"
+
